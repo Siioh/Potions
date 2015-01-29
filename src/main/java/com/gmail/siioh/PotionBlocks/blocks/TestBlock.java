e241191c009012eb72a1c6e4c46f3c8db32294f3
Siioh <ramsarrantyler@gmail.com>
2015-01-28 22:47:41 -0500

    Altered sound

--- src/main/java/com/gmail/siioh/PotionBlocks/blocks/TestBlock.java
+++ src/main/java/com/gmail/siioh/PotionBlocks/blocks/TestBlock.java
@@ -10,10 +10,9 @@ public class TestBlock extends PotionBlock {
 	public TestBlock(Material material) {
 		super(material);
 		setHardness(0.9F);
-        setStepSound(Block.soundTypeAnvil);
+        setStepSound(Block.soundTypeStone);
         setBlockName("jthrBlock");
         setCreativeTab(PotionCreativeTab.tab);
         setBlockTextureName("potions:jthrBrick");
 	}
 }
-		
